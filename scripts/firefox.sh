e4ea1b69acf582b037630c4b7c9ee350c4ea7ce1
M0-7 <moazhaider7@protonmail.com>
2024-03-01 19:22:47 +0000

    Update firefox script

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 #Profile shorthands
-personal=gsatmo6f.default-release
-study=1dfz6q9z.Study
+personal=x33nh28d.default-release
+study=bqr5ekof.Study
 url=https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
 
 #Update user.js
 curl $url | tee ~/.mozilla/firefox/$personal/user.js ~/.mozilla/firefox/$study/user.js
 echo "Done updating user.js"
+
+#Update firefox gnome theme
+curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
 
